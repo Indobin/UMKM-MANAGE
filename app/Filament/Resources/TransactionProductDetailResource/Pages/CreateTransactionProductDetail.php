492ae29ffbe8ceb4b7cd0a7e7bc7ff899f6c45a7
Satya788. <fegotero7@gmail.com>
2024-11-26 23:20:13 +0700

    fix transaction

--- app/Filament/Resources/TransactionProductDetailResource/Pages/CreateTransactionProductDetail.php
+++ app/Filament/Resources/TransactionProductDetailResource/Pages/CreateTransactionProductDetail.php
@@ -6,8 +6,11 @@ use Filament\Actions;
 use App\Models\Product;
 // use Filament\Forms\Components\Actions\Action;
 use Filament\Actions\Action;
+use Illuminate\Support\Facades\DB;
+use App\Models\TransactionProductDetail;
 use Filament\Resources\Pages\CreateRecord;
 use App\Filament\Resources\TransactionProductDetailResource;
+use App\Filament\Resources\TransactionProductDetailResource\Widgets\TransactionDetailWidget;
 
 class CreateTransactionProductDetail extends CreateRecord
 {
@@ -16,10 +19,11 @@ class CreateTransactionProductDetail extends CreateRecord
     {
         return [
             Action::make('create')
-                ->label('Simpan')
+                ->label('Tambah')
                 ->submit('create')
                 ->keyBindings(['mode+s']),
         ];
+
     }
     protected function getRedirectUrl(): string
     {
@@ -29,45 +33,46 @@ class CreateTransactionProductDetail extends CreateRecord
         [
             'transaction_id' => $id
         ]);
-    
+
     }
-    // protected function afterSave(): void
-    // {
-    //     // Ambil produk terkait
-    //     $product = Product::find($this->record->product_id);
+    public function getFooterWidgetsColumns(): int | array
+    {
+        return 1;
+    }
+    public function getFooterWidgets():array
+    {
+        return [
+            TransactionDetailWidget::make([
+                'record' => request('transaction_id')
+            ]),
+        ];
+    }
 
-    //     if ($product) {
-    //         // Periksa apakah stok cukup
-    //         if ($product->stok < $this->record->jumlah) {
-    //             throw new \Exception('Jumlah yang dimasukkan melebihi stok yang tersedia.');
-    //         }
-
-    //         // Kurangi stok produk
-    //         $product->decrement('stok', $this->record->jumlah);
-    //     }
-    // }
     protected function beforeSave(): void
     {
-      // Contoh validasi untuk memastikan 'stok' adalah angka sebelum melakukan operasi
-$product = Product::find($this->record->product_id);
 
-if ($product) {
-    // Memastikan 'stok' adalah angka sebelum manipulasi
-    $stok = $product->stok;
+    $product = Product::find($this->record->product_id);
 
-    if (!is_numeric($stok)) {
-        throw new \Exception('Stok harus berupa angka yang valid.');
+    if ($product) {
+
+        $stok = $product->stok;
+
+        if (!is_numeric($stok)) {
+            throw new \Exception('Stok harus berupa angka yang valid.');
+        }
+
+        if ($this->record->jumlah > $stok) {
+            $this->notify('danger', 'Jumlah yang dimasukkan melebihi stok yang tersedia!');
+            throw new \Exception('Jumlah tidak valid.');
+        }
+
+        $product->decrement('stok', $this->record->jumlah);
     }
 
-    if ($this->record->jumlah > $stok) {
-        $this->notify('danger', 'Jumlah yang dimasukkan melebihi stok yang tersedia!');
-        throw new \Exception('Jumlah tidak valid.'); // Menghentikan proses penyimpanan
     }
 
-    // Kurangi stok jika valid
-    $product->decrement('stok', $this->record->jumlah);
-}
 
-    }
+
+
 
 }
